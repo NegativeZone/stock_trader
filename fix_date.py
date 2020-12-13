655e86d965e751536511aefb2c626c5f51ba1078
Anuroop Bisaria <anuroop@bisaria.com>
2020-12-13 21:26:18 +0530

    Fixed dates on BSE data, Fixed column headings on NSE data (code only)

--- fix_date.py
+++ fix_date.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import glob
+from os.path import basename, splitext
 
-index_frame = pd.read_csv('dataset/nse/index/NIFTY50.csv')
-index_frame['Date'] = pd.to_datetime(index_frame['Date'], format='%d-%b-%Y')
-index_frame.to_csv('dataset/nse/index/NIFTY50_datefix.csv', index=False)
+# index_frame = pd.read_csv('dataset/nse/index/NIFTY50.csv')
+# index_frame['Date'] = pd.to_datetime(index_frame['Date'], format='%d-%b-%Y')
+# index_frame.to_csv('dataset/nse/index/NIFTY50_datefix.csv', index=False)
+
+filenames = glob.glob('dataset/bse/stocks/*.csv')
+dfs_bse = {splitext(basename(fp))[0]: pd.read_csv(fp) for fp in filenames}
+
+for i in dfs_bse:
+    dfs_bse[i]['Date'] = pd.to_datetime(dfs_bse[i]['Date'], format='%d-%B-%Y')
+    dfs_bse[i].to_csv('dataset/bse/stocks/'+i+'.csv', index=False)
--- read_stocks.py
+++ read_stocks.py
@@ -3,6 +3,21 @@ import glob
 from os.path import basename, splitext
 
 filenames = glob.glob('dataset/nse/stocks/*.csv')
-dfs = {splitext(basename(fp))[0]: pd.read_csv(fp) for fp in filenames}
+dfs_nse = {splitext(basename(fp))[0]: pd.read_csv(fp) for fp in filenames}
 
-print(dfs['BAJAJ-AUTO'].columns)
+for i in dfs_nse:
+    dfs_nse[i] = dfs_nse[i].rename(
+        columns={"%Deliverble": "Percent Deliverable"})
+
+filenames = glob.glob('dataset/bse/stocks/*.csv')
+dfs_bse = {splitext(basename(fp))[0]: pd.read_csv(fp) for fp in filenames}
+
+# print(dfs['BAJAJ-AUTO'].iloc[0])
+
+# for i in dfs:
+#     for j in dfs[i].columns:
+#         dfs[i][j] = dfs[i][j].interpolate(
+#             method='linear',
+#             limit_direction='backward')
+#     print(i + ": " + str(dfs[i].shape[0] - dfs[i].dropna().shape[0]))
+#     print(dfs[i])
