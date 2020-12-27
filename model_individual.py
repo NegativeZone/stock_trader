49bead257086cd16ac93ea8278f4d685479da501
Anuroop Bisaria <anuroop@bisaria.com>
2020-12-27 18:25:30 +0530

    Created row of closing prices for the past week

--- model_individual.py
+++ model_individual.py
@@ -13,6 +13,11 @@ for i in filenames:
     dfs[i]['Movement'] = dfs[i]['Change'].apply(
         lambda x: 1 if x > 0 else 0)
     dfs[i] = dfs[i].sort_values(by="Date")
-    dfs[i] = dfs[i].reset_index(drop=True)
     dfs[i] = dfs[i].drop(columns=["Date", "Symbol"])
+    for j in range(5):
+        print(j)
+        dfs[i]["CD-"+str(j+1)] = dfs[i]["Close"].shift(j+2)
+    dfs[i] = dfs[i].dropna()
+    dfs[i] = dfs[i].drop(columns="Prev Close")
+    dfs[i] = dfs[i].reset_index(drop=True)
     print(dfs[i])
